c230c0c29c8d3b767ab70338f1cc58d3118afbbe
Peter Daniel Samuel <petiibhuzah@gmail.com>
2018-05-27 16:54:07 +0300

    changing a little bit the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header text-center"> Welcome</div>
+                <div class="card-header text-center"> Welcome ,{{ Auth::user()->name }} </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Welcome To Our Site , {{ Auth::user()->name }}
+                    Welcome To Our Site , click <a href="{{route('index')}}">Here to go Home page</a>
                 </div>
             </div>
         </div>
